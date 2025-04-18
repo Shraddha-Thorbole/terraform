775e71c2d82c3f881878bc754a849d70f828483a
Shraddha-Thorbole <shraddhathorbole88@gmail.com>
2025-04-18 19:13:36 +0530

    EXP-7

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+region         = "ap-south-1"
+ami_id         = "ami-03bb6d83c60fc5f7c"
+instance_type  = "t2.micro"
+
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+
+
+
